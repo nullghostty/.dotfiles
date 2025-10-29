f834c5410671da9c7a5b108b15d01104e6504279
ulises-gomez-dev <ulises.gomez.dev@proton.me>
2025-10-28 21:36:41 -0700

    changed theme to nordic (https://github.com/AlexvZyl/nordic.nvim)

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,7 +3,6 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set number")
-vim.cmd("set autoread")
 -- vim.cmd("set relativenumber")
 
 -- Navigate vim panes better
